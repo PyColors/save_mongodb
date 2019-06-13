3c0c83eb52e8ac060b2bb1614aa3057bd242ccc3
PyColors <patweb44@gmail.com>
2019-06-13 23:03:20 +0100

    Update app.js and index.html

--- app.js
+++ app.js
@@ -3,18 +3,19 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const mongodb = require('mongodb');
 
+const app = express();
+app.use(express.static(path.resolve(__dirname, 'public')));
+
+
 const dbConn = mongodb.MongoClient.connect('mongodb://localhost:27017', {
     useNewUrlParser: true
 });
 
 
-const app = express();
-
 app.use(bodyParser.urlencoded({
     extended: false
 }));
 
-app.use(express.static(path.resolve(__dirname, 'public')));
 
 app.post('/post-thanks', function(req, res) {
     dbConn.then(function(db) {
@@ -26,8 +27,8 @@ app.post('/post-thanks', function(req, res) {
 
 app.get('/thanks', function(req, res) {
     dbConn.then(function(db) {
-        db.collection('feedbacks').find({}).toArray().then(function(feedbacks) {
-            res.status(200).json(feedbacks);
+        db.collection('information').find({}).toArray().then(function(information) {
+            res.status(200).json(information);
         });
     });
 });
